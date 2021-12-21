d41322f37d86bf5b39e0fcc5264285c47dcffa76
Jefin <jefersonramosoliveira8@gmail.com>
2021-12-20 23:57:57 -0300

    feat: navbar [stable]

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <template>
-    <div>
-        
+    <NavBar></NavBar>
+    <div class="content">
+        <nav>
+            <img src="./assets/background.svg" alt="">
+            <div class="container_ball">
+                <div class="ball"></div>
+            </div>
+        </nav>
     </div>
 </template>
 
 <script>
+import NavBar from "./components/NavBar/NavBar.vue";
 export default {
     name: "App",
+
+    components: { NavBar },
 };
 </script>
---
+++ src/components/NavBar/NavBar.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="nav-bar">
+        <div class="title">
+            Jeferson R.
+        </div>
+
+        <div class="menu">
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+<script>
+import './NavBar.css';
+
+export default {
+    
+}
+</script>
